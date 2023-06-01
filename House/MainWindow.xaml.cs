af2a7e48b3f3bd71d9e5f2c96f03ccbb8ff47910
AnnRudneva <119349516+AnnRudneva@users.noreply.github.com>
2023-06-01 21:06:13 +0300

    Add files via upload

---
+++ House/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RoomLibrary1;
+
+namespace House
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Room room = new Room();
+        LivingRoom livingRoom = new LivingRoom();
+        Office office= new Office();
+        List<Room> lstRooms = new List<Room>();//создание списка комнат
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BaddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            Room room = new Room();
+            room.RoomLenght = Convert.ToDouble(TBLenghtR.Text);
+            room.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+            lstRooms.Add(room); //добавление в список
+        }
+
+        private void BaddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            LivingRoom livingRoom = new LivingRoom();
+            livingRoom.RoomLenght = Convert.ToDouble(TBLenghtL.Text);
+            livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+            livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+            lstRooms.Add(livingRoom); //добавление в список
+        }
+
+        private void BaddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            Office office = new Office();
+            office.RoomLenght = Convert.ToDouble(TBLenghtO.Text);
+            office.RoomWidth = Convert.ToDouble(TBWidthO.Text);
+            office.NumSocket = Convert.ToInt32(TBNumS.Text);
+            lstRooms.Add(office); //добавление в список
+        }
+
+        private void BGetList_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content = "";
+            //вывод информации из списка
+            foreach (Room r in lstRooms)
+                ListRooms.Content += r.Info() + "\n";
+        }
+    }
+}
---
+++ RoomLibrary1/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary1
+{
+    public class Room
+    {
+        double roomLenght; //длина комнатыв
+        double roomWidth; //ширина комнаты
+
+        public double RoomLenght
+        {
+            get { return roomLenght; }
+            set { roomLenght = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        /// <summary>
+        /// Метод вычисляет периметр комнаты
+        /// </summary>
+        /// <returns>Возвращает значение периметра</returns>
+        public double RoomPerimeter()
+        {
+            return 2 * (roomLenght + roomWidth);
+        }
+        /// <summary>
+        /// Метод вычисляет площадь комнаты 
+        /// </summary>
+        /// <returns>Возвращает значение площади</returns>
+        public double RoomArea()
+        {
+            return roomLenght * roomWidth;
+        }
+        public double PersonalArea(int np)
+        {
+            return RoomArea() / np;
+        }
+        /// <summary>
+        /// Информация о комнате
+        /// </summary>
+        /// <returns>Возвращаеь строку</returns>
+        public virtual string Info()//virtual - при необходимости может быть переопределен
+        {
+            return "Комната площадью " + RoomArea() + " кв.м";
+        }
+    }
+    /// <summary>
+    /// Класс жилая комната
+    /// </summary>
+    public class LivingRoom : Room
+    {
+        int numWin; //число окон
+        public int NumWin
+        { get { return numWin; } set { numWin = value; } }
+
+        /// <summary>
+        /// Метод возвращает информацию о коинате
+        /// </summary>
+        /// <returns>Возвращвет строку с информацией</returns>
+        public  override string Info()   //override-метод производного класса преобладает над методом базового
+        {
+            return "Жилая комната площадью " + RoomArea() + " кв.м , с " + numWin + " окнами";
+        }
+    }
+    public class Office : Room
+    {
+        int numSocket; //число розеток
+        public int NumSocket
+        { get { return numSocket; } set { numSocket = value; } }
+        /// <summary>
+        /// Возвращает максимально возможное число рабочих мест
+        /// </summary>
+        /// <returns>Возвращает число мест</returns>
+        public int NumWorkplaces()
+        {
+            int num = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5)) ;//число возможных раб. мест без учета розеток
+            return Math.Min(numSocket, num);
+        }
+        /// <summary>
+        /// Метод возвращает информацию об офисе
+        /// </summary>
+        /// <returns>Возвращается строка с информацией</returns>
+        public override string Info()
+        {
+            return "Офис на " + NumWorkplaces() + " рабочих мест";
+        }
+    }
+}
